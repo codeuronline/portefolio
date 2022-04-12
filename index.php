77906ebbd07450ad97f97459808b4e6f109297dc
codeuronline <j.kasperski@codeur.online>
2022-04-12 13:03:07 +0200

    update cube

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
 
     <!-- Template Main CSS File -->
     <link href="assets/css/style.css" rel="stylesheet">
-    <link rel="stylesheet" href="public/css/style.css">
+    <!-- <link rel="stylesheet" href="public/css/style.css"> -->
     <!-- =======================================================
   * Template Name: iPortfolio - v3.7.0
   * Template URL: https://bootstrapmade.com/iportfolio-bootstrap-portfolio-websites-template/
@@ -142,13 +142,12 @@
 
                 <div class="section-title">
                     <h2>A propos</h2>
-                    <p>Expert dans le développement Web et Web Mobile. Professionnel doté d'un grand
-                        sens de l'organisation et d'une solide connaissance dans le domaine du Web, je
+                    <p>Développeur Web et Web Mobile. Professionnel doté d'un grand
+                        sens de l'organisation et d'une solide connaissance dans le domaine de l'Informatique, je
                         désire évoluer au sein de ce secteur. Je sais m'intégrer sans difficultés dans de
                         nouveaux contextes de travail grace à un véritable esprit d'équipe et à d'excellentes
                         capacités d'écoute, de flexibilité, et fais preuve d'une grande autonomie</p>
                 </div>
-
                 <div class="row">
                     <div class="col-lg-4" data-aos="fade-right">
                         <img src="assets/img/profile-img.jpg" class="img-fluid" alt="">
@@ -332,15 +331,24 @@
                         </div>
 
                         <div class="progress">
-                            <span class="skill">WordPress/CMS <i class="val">90%</i></span>
+                            <span class="skill">WordPress/CMS <i class="val">55%</i></span>
                             <div class="progress-bar-wrap">
-                                <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0"
+                                <div class="progress-bar" role="progressbar" aria-valuenow="55" aria-valuemin="0"
+                                    aria-valuemax="100"></div>
+                            </div>
+                        </div>
+
+
+                        <div class="progress">
+                            <span class="skill">Suite Office</span>
+                            <div class="progress-bar-wrap">
+                                <div class="progress-bar" role="progressbar" aria-valuenow="55" aria-valuemin="0"
                                     aria-valuemax="100"></div>
                             </div>
                         </div>
 
                         <div class="progress">
-                            <span class="skill">Photoshop <i class="val">55%</i></span>
+                            <span class="skill">Suite Adobe</span>
                             <div class="progress-bar-wrap">
                                 <div class="progress-bar" role="progressbar" aria-valuenow="55" aria-valuemin="0"
                                     aria-valuemax="100"></div>
